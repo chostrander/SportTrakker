c1034eed45f15eb241951d6f87ac193fcfe7d64c
Carl <carl.hostrander00@gmail.com>
2021-02-15 21:41:27 -0600

    cleaning up and adding new fixes

--- ReadGoogleSheets/src/main/java/org/hoss/RefTrakkerMain.java
+++ ReadGoogleSheets/src/main/java/org/hoss/RefTrakkerMain.java
@@ -1,3 +1,5 @@
+package org.hoss;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -5,9 +7,9 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
 
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.*;
@@ -20,9 +22,13 @@ import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.util.*;
 
-public class QuickStart {
+@SpringBootApplication
+public class RefTrakkerMain {
 
     /** Application name. */
     private static final String APPLICATION_NAME =
@@ -63,7 +69,7 @@ public class QuickStart {
     private static Credential authorize() throws IOException {
         // Load client secrets.
         InputStream in =
-                QuickStart.class.getResourceAsStream("/client_secret.json");
+                RefTrakkerMain.class.getResourceAsStream("/client_secret.json");
         GoogleClientSecrets clientSecrets =
                 GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
@@ -89,6 +95,9 @@ public class QuickStart {
     }
 
     public static void main(String[] args) throws IOException, ParseException {
+
+        SpringApplication.run(RefTrakkerMain.class, args);
+
         // Build a new authorized API client service.
         Sheets service = getSheetsService();
 
@@ -96,21 +105,51 @@ public class QuickStart {
         // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
         //https://docs.google.com/spreadsheets/d/10f4E_6B9NBZglLAVaSPqsz--z0jNcZ3ltCOIPwB7EEI/edit?ts=5a4ec4cd#gid=0
         String spreadsheetId = "10f4E_6B9NBZglLAVaSPqsz--z0jNcZ3ltCOIPwB7EEI";
-        String range = "Schedule!A1:E";
+        String range = "Schedule!A1:E27";
         printRange(range,spreadsheetId,service);
-        range = "Schedule!H1:L";
+        range = "Schedule!H1:L27";
         printRange(range,spreadsheetId,service);
-        range = "Schedule!O1:S";
+        range = "Schedule!O1:S27";
         printRange(range,spreadsheetId,service);
-        range = "Schedule!V1:Z";
+        range = "Schedule!V1:Z27";
         printRange(range,spreadsheetId,service);
-        range = "Schedule!AC1:AG";
+        range = "Schedule!AC1:AG27";
         printRange(range,spreadsheetId,service);
-        range = "Schedule!AJ1:AN";
+        range = "Schedule!AJ1:AN27";
         printRange(range,spreadsheetId,service);
-        range = "Schedule!AQ1:AU";
+        range = "Schedule!AQ1:AU27";
         printRange(range,spreadsheetId,service);
 
+
+range = "Schedule!A28:E54";
+ printRange(range,spreadsheetId,service);
+ range = "Schedule!H28:L54";
+ printRange(range,spreadsheetId,service);
+ range = "Schedule!O28:S54";
+ printRange(range,spreadsheetId,service);
+ range = "Schedule!V28:Z54";
+ printRange(range,spreadsheetId,service);
+ range = "Schedule!AC28:AG54";
+ printRange(range,spreadsheetId,service);
+ range = "Schedule!AJ28:AN54";
+ printRange(range,spreadsheetId,service);
+ range = "Schedule!AQ28:AU54";
+ printRange(range,spreadsheetId,service);
+
+        range = "Schedule!A55:E81";
+        printRange(range,spreadsheetId,service);
+        range = "Schedule!H55:L81";
+        printRange(range,spreadsheetId,service);
+        range = "Schedule!O55:S81";
+        printRange(range,spreadsheetId,service);
+        range = "Schedule!V55:Z81";
+        printRange(range,spreadsheetId,service);
+        range = "Schedule!AC55:AG81";
+        printRange(range,spreadsheetId,service);
+        range = "Schedule!AJ55:AN81";
+        printRange(range,spreadsheetId,service);
+        range = "Schedule!AQ55:AU81";
+        printRange(range,spreadsheetId,service);
     }
 
     private static void printRange(String range, String spreadSheetId, Sheets service) throws IOException, ParseException {
@@ -123,20 +162,22 @@ public class QuickStart {
         if (values == null || values.size() == 0) {
             System.out.println("No data found.");
         } else {
-            System.out.println("Processing Mondays");
-            System.out.println("time,game,ref");
+            Date gameDate;
+            System.out.println("Processing...");
             for (List row : values) {
-                if (row.size() == 1 && ((String)row.get(0)).startsWith("American Indoor")) {
+                if (row.size() == 1 && ((String) row.get(0)).endsWith("DAY")) {
                     System.out.println(row.get(0));
-                } else if (row.size() == 1 && ((String)row.get(0)).endsWith("DAY")) {
-                    System.out.println(row.get(0));
-                } else if (row.size() == 1 && ((String)row.get(0)).matches("[a-zA-z]*\\s\\d{1,2},\\s\\d{4}")) {
-                    Date date = sFormatter.parse(row.get(0).toString());
-
-                    System.out.println("Date--> "+ date.toString());
-                } else if (row.size() == 5 && !((String)row.get(0)).equalsIgnoreCase("TIMES")) {
-                    System.out.printf("%s\t%s\t%s\n",row.get(0),row.get(4),row.get(2));
-                } else if (row.size() == 3 && ((String)row.get(2)).contains(" v ")) {
+                } else if (row.size() == 1 && ((String) row.get(0)).matches("[a-zA-z]*\\s\\d{1,2},\\s\\d{4}")) {
+                    gameDate = sFormatter.parse(row.get(0).toString());
+                    System.out.println("Date--> " + sFormatter.format(gameDate));
+                } else if (row.size() == 5 && !((String) row.get(0)).startsWith("TIMES")) {
+                    System.out.printf("%s\t%15s\t%30s\t%s\t%s\n", row.get(0), row.get(1), row.get(3), row.get(4), row.get(2));
+                } else if (row.size() == 3 &&
+                        (((String) row.get(2)).startsWith("Expected") || ((String) row.get(2)).endsWith("Coed Playoffs") || ((String) row.get(2)).contains("Mens Playoffs"))) {
+                    System.out.printf("%s\tNo Ref\t%s\n", row.get(0), row.get(2));
+                } else if (row.size() == 3 && ((String)row.get(2)).contains("Coed")) {
+                    System.out.printf("%s\tNo Ref\t%s\n", row.get(0), row.get(2));
+                }else if (row.size() == 3 &&  ((String)row.get(2)).contains(" v ")) {
                     System.out.printf("%s\tNo Ref\t%s\n",row.get(0),row.get(2));
                 }
 
---
+++ ReadGoogleSheets/src/test/java/org/hoss/RefTrakkerMainTests.java
@@ -0,0 +1,12 @@
+package org.hoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RefTrakkerMainTests {
+
+    @Test
+  void contextLoads() {
+  }
+}
